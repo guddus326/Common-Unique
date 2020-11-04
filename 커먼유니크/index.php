80d84578431b3cb4200de8b28620fe22c12cf99b
HyeongYeon Ham <46678986+guddus326@users.noreply.github.com>
2020-11-04 15:50:31 +0900

    Add files via upload

---
+++ 커먼유니크/index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>커먼유니크</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500;1,800&display=swap" rel="stylesheet">
+    <style>
+        body { padding-top: 50px; }
+        .navbar-fixed-top { background-color: #ffffff; }
+        .page-header{
+            font-family: 'Playfair Display', serif;
+            text-align: left;
+        }
+        .caption{
+            font-size: x-small;
+        }
+        .pop{
+            text-align:center;
+        }
+        .notice{
+            text-align: center;
+        }
+        .footer{
+            text-align: right;
+        }
+       
+       
+       
+      
+         
+    </style>
+  </head>
+  <body>
+
+    <nav class="navbar-fixed-top navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+                  </a>
+              </div>
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" style="margin-top:-10px" src="./img/로고.JPG">
+                  </a>
+              </div>
+             
+      
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"> <span class="sr-only">(current)</span></li>
+              <li><a href="#">outer</a></li>
+              <li><a href="#">shirt&blouse</a></li>
+              <li><a href="#">knit&cardigan</a></li>
+              <li><a href="#">top</a></li>
+              <li><a href="#">pants</a></li>
+              <li><a href="#">skirt</a></li>
+              <li><a href="#">bag</a></li>
+              <li><a href="#">shoes</a></li>
+              <li><a href="#">acc</a></li>
+              <li><a href="#">jewelry</a></li>
+            </ul>
+            
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">LOGIN</a></li>
+              <li><a href="#">JOIN</a></li>
+              <li><a href="#">CART</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+      
+                <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img width="100%";height="auto"; src="./img/메인.JPG" alt="...">
+                </div>
+                <div class="item">
+                    <img width="100%";height="auto"; src="./img/메인.JPG" alt="...">
+                </div>
+            <div class="item">
+                <img width="100%";height="auto"; src="./img/메인.JPG" alt="...">
+            </div>
+            </div>
+        
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </a>
+      </div>
+     
+    <br><br><br><br><br><br>
+      <div class="notice"> 
+        <img src="./img/게시판1.JPG">
+        <img src="./img/게시판2.JPG">
+        <img src="./img/게시판3.JPG">
+      <div>
+    <br><br><br><br>
+
+      <div class="page-header">
+        <h1>&nbsp;&nbsp;The most popular products</h1>
+      </div>
+    <div class="pop">
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                   
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                    
+                </div>
+        </div>
+    </div>
+
+    &nbsp;&nbsp;&nbsp;
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br>
+                </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="col-sm-5 col-md-3">
+                <img src="./img/사진1.JPG" alt="...">
+                <div class="caption">
+                    <h6>[OUTER] PENO COLORING KNIT CARDIGAN</h6><br>
+                    18,000 won<br>
+                    review : 11<br><br>
+                    심플한 무드가 느껴지는 스탠다드 핏의 니트 가디건<br>
+                    클로징 라인에 배색 디테일을 더해 밋밋하지 않습니다<br><br><br><br><br>
+                </div>
+        </div>
+</div>
+   <div class="middle">
+       <img width="100%";height="auto"; src="./img/중간.JPG"><br><br><br>
+   </div>
+    <div class="pop">
+        <div class="col">
+            <div class="col-sm-6 col-md-4">
+                  <p>CUSTOMER SERVICE<br>
+                    1688-9308<br>
+                    AM 10:00 - PM5:00 MON-FRI (HOLIDAY OFF)<br>
+                    LUNCH PM12:00 - 1:00<br></p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-sm-6 col-md-4">
+                  <p>BANK ACCOUNT<br>
+                    농협301-0220-0838-21<br>
+                    국민039001-04-245314<br>
+                    (주)어나더무드<br>
+                    </p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-sm-6 col-md-4">
+                  <p>RETURN<br>
+                    반품주소<br>
+                    CJ대한통운 이용시 : 서울 동작구 대방동 CJ 대한통운 대방대리점<br>
+                    타택배 이용시 : 서울시 금천구 독산동 291-1 현대지식산업센터 B동 2304호<br><br></p>
+            </div>
+        </div>
+        </div>
+        <div class="middle">
+            <img width="100%";height="auto"; src="./img/인스타.JPG"><br><br>
+        </div>
+
+        <div class="footer">
+            <div class="col">
+                    ABOUT COMPANY &nbsp;&nbsp;&nbsp;
+                    TERMS OF USE &nbsp;&nbsp;&nbsp;
+                    USER GUIDE &nbsp;&nbsp;&nbsp;
+                    개인정보 취급방침<br> 
+                    TRADE NAME &nbsp;커먼유니크&nbsp;|&nbsp;
+                    COMPANY&nbsp; (주)어나더무드&nbsp;|&nbsp;
+                    OWNER  &nbsp;손영일&nbsp;|&nbsp;
+                    BUSINESS LICENSE &nbsp; 108-81-98622&nbsp;|&nbsp; 
+                    ONLINE BUSINESS LICENSE  &nbsp;제2019-서울마포-2200호&nbsp;<br>
+
+                    ADDRESS  &nbsp;서울특별시 마포구 동교로23길 32-23&nbsp;|&nbsp;
+                    CALL CENTER  &nbsp;1688-9308&nbsp;|&nbsp;
+                    E-MAIL  &nbsp;help@anothermood.com&nbsp;|&nbsp;
+                    제휴문의  &nbsp;yison@anothermood.com&nbsp;<br><br>
+            </div>
+        </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+  </body>
+</html>
